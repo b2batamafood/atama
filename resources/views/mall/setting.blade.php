4075fdb67cfdd2798ab0c3673ed28f8275805885
Calvin Christiano <calvchristiano@gmail.com>
2024-01-05 23:07:53 +0700

    fixing product modal and view responsive

--- resources/views/mall/setting.blade.php
+++ resources/views/mall/setting.blade.php
@@ -3,12 +3,12 @@
     <div class="px-10 lg:px-40 py-10 mx-auto">
 
         {{-- EDIT FIELD START --}}
-        <div class="flex mb-14">
-            <div class="w-1/3">
-                <h1 class="capitalize font-bold text-lg">personal information</h1>
+        <div class="flex-none md:flex mb-14">
+            <div class="w-full md:w-1/3 mb-5 md:mb-0">
+                <h1 class="capitalize font-bold text-base md:text-lg">personal information</h1>
             </div>
 
-            <div class="w-2/3">
+            <div class="w-full md:w-2/3">
                 {{-- Success Notification --}}
                 @if (session()->has('successChangeProfile'))
                     <div id="alert-3"
@@ -39,51 +39,51 @@
                     @csrf
                     <div class="flex mb-5">
                         <div class="w-1/3 mr-4">
-                            <label for="firstname" class="text-gray-600 mb-2 block font-semibold capitalize">first
+                            <label for="firstname" class="text-gray-600 mb-2 block font-semibold capitalize text-sm md:text-base">first
                                 name</label>
                             <input type="text" name="firstname" id="firstname"
-                                   class="block w-full border border-gray-300 px-4 py-3 text-gray-600 text-sm rounded"
+                                   class="block w-full border border-gray-300 px-4 py-2 md:py-3 text-gray-600 text-sm rounded"
                                    required>
                         </div>
                         <div class="w-1/3 mx-4">
-                            <label for="lastname" class="text-gray-600 mb-2 block font-semibold capitalize">last
+                            <label for="lastname" class="text-gray-600 mb-2 block font-semibold capitalize text-sm md:text-base">last
                                 name</label>
                             <input type="text" name="lastname" id="lastname"
-                                   class="block w-full border border-gray-300 px-4 py-3 text-gray-600 text-sm rounded"
+                                   class="block w-full border border-gray-300 px-4 py-2 md:py-3 text-gray-600 text-sm rounded"
                                    required>
                         </div>
                         <div class="w-1/3 ml-4">
-                            <label for="company" class="text-gray-600 mb-2 block font-semibold capitalize">company</label>
+                            <label for="company" class="text-gray-600 mb-2 block font-semibold capitalize text-sm md:text-base">company</label>
                             <input type="text" name="company" id="company"
-                                   class="block w-full border border-gray-300 px-4 py-3 text-gray-600 text-sm rounded"
+                                   class="block w-full border border-gray-300 px-4 py-2 md:py-3 text-gray-600 text-sm rounded"
                                    required>
                         </div>
                     </div>
                     <div class="flex mb-5">
                         <div class="w-1/2 mr-4">
-                            <label for="email" class="text-gray-600 mb-2 block font-semibold capitalize">email</label>
+                            <label for="email" class="text-gray-600 mb-2 block font-semibold capitalize text-sm md:text-base">email</label>
                             <input type="email" name="email" id="email"
-                                   class="block w-full border border-gray-300 px-4 py-3 text-gray-600 text-sm rounded placeholder-gray-400"
+                                   class="block w-full border border-gray-300 px-4 py-2 md:py-3 text-gray-600 text-sm rounded placeholder-gray-400"
                                    placeholder="youremail@domain.com" required>
                         </div>
                         <div class="w-1/2 ml-4">
-                            <label for="phone" class="text-gray-600 mb-2 block font-semibold capitalize">phone</label>
+                            <label for="phone" class="text-gray-600 mb-2 block font-semibold capitalize text-sm md:text-base">phone</label>
                             <input type="text" name="phone" id="phone"
-                                   class="block w-full border border-gray-300 px-4 py-3 text-gray-600 text-sm rounded"
+                                   class="block w-full border border-gray-300 px-4 py-2 md:py-3 text-gray-600 text-sm rounded"
                                    required>
                         </div>
                     </div>
                     <div class="flex mb-5">
                         <div class="w-full">
-                            <label for="address" class="text-gray-600 mb-2 block font-semibold capitalize">address</label>
+                            <label for="address" class="text-gray-600 mb-2 block font-semibold capitalize text-sm md:text-base">address</label>
                             <input type="text" name="address" id="address"
-                                   class="block w-full border border-gray-300 px-4 py-3 text-gray-600 text-sm rounded"
+                                   class="block w-full border border-gray-300 px-4 py-2 md:py-3 text-gray-600 text-sm rounded"
                                    required>
                         </div>
                     </div>
                     <div class="flex">
                         <button type="submit" name="editField"
-                                class="w-16 py-1 text-center text-white bg-primary border border-primary rounded hover:bg-transparent hover:text-primary transition capitalize font-medium text-base">save</button>
+                                class="w-16 py-1 text-center text-white bg-primary border border-primary rounded hover:bg-transparent hover:text-primary transition capitalize font-medium text-sm md:text-base">save</button>
                     </div>
                 </form>
             </div>
@@ -93,13 +93,13 @@
         <div class="border-2 w-full"></div>
 
         {{-- CHANGE PASSWORD START --}}
-        <div class="flex mt-14">
-            <div class="w-1/3 pr-14">
-                <h1 class="capitalize font-bold text-lg">change password</h1>
-                <p class="text-base">Update your password associated with your account.</p>
+        <div class="flex-none md:flex mt-14">
+            <div class="w-full md:w-1/3 pr-0 md:pr-14 mb-5 md:mb-0">
+                <h1 class="capitalize font-bold text-base md:text-lg">change password</h1>
+                <p class="text-sm md:text-base">Update your password associated with your account.</p>
             </div>
 
-            <div class="w-2/3">
+            <div class="w-full md:w-2/3">
                 {{-- Failed Notification --}}
                 @if (session()->has('currentpwError'))
                     <div id="alert-2"
@@ -155,10 +155,10 @@
                 <form action="/setting" method="POST" autocomplete="off">
                     @csrf
                     <div class="mb-5">
-                        <label for="current" class="text-gray-600 mb-2 block font-semibold capitalize">current
+                        <label for="current" class="text-gray-600 mb-2 block font-semibold capitalize text-sm md:text-base">current
                             password</label>
                         <input type="text" name="current" id="current"
-                               class="block w-full border border-gray-300 px-4 py-3 text-gray-600 text-sm rounded" required
+                               class="block w-full border border-gray-300 px-4 py-2 md:py-3 text-gray-600 text-sm rounded" required
                                autofocus>
                         @error('current')
                         <p id="standard_error_help" class="mt-2 text-xs text-red-600 dark:text-red-400">
@@ -167,10 +167,10 @@
                         @enderror
                     </div>
                     <div class="mb-5">
-                        <label for="new" class="text-gray-600 mb-2 block font-semibold capitalize">new
+                        <label for="new" class="text-gray-600 mb-2 block font-semibold capitalize text-sm md:text-base">new
                             password</label>
                         <input type="text" name="new" id="new"
-                               class="block w-full border border-gray-300 px-4 py-3 text-gray-600 text-sm rounded" required>
+                               class="block w-full border border-gray-300 px-4 py-2 md:py-3 text-gray-600 text-sm rounded" required>
                         @error('new')
                         <p id="standard_error_help" class="mt-2 text-xs text-red-600 dark:text-red-400">
                             {{ $message }}
@@ -178,10 +178,10 @@
                         @enderror
                     </div>
                     <div class="mb-5">
-                        <label for="confirmnew" class="text-gray-600 mb-2 block font-semibold capitalize">confirm
+                        <label for="confirmnew" class="text-gray-600 mb-2 block font-semibold capitalize text-sm md:text-base">confirm
                             password</label>
                         <input type="text" name="confirmnew" id="confirmnew"
-                               class="block w-full border border-gray-300 px-4 py-3 text-gray-600 text-sm rounded" required>
+                               class="block w-full border border-gray-300 px-4 py-2 md:py-3 text-gray-600 text-sm rounded" required>
                         @error('confirmnew')
                         <p id="standard_error_help" class="mt-2 text-xs text-red-600 dark:text-red-400">
                             {{ $message }}
@@ -189,7 +189,7 @@
                         @enderror
                     </div>
                     <button type="submit" name="changePassword"
-                            class="w-16 py-1 text-center text-white bg-primary border border-primary rounded hover:bg-transparent hover:text-primary transition capitalize font-medium text-base">save</button>
+                            class="w-16 py-1 text-center text-white bg-primary border border-primary rounded hover:bg-transparent hover:text-primary transition capitalize font-medium text-sm md:text-base">save</button>
                 </form>
             </div>
         </div>
